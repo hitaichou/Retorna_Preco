67c3728315d8d9dc6ae39a479223f87b6133036c
Raphael Fernandes <holyfernandes@yahoo.co.uk>
2020-05-19 19:10:38 -0300

    Criação da classe produto e calculo do preço unitário

---
+++ Retorna_Preco/Produto.cs
@@ -0,0 +1,132 @@
+﻿using System.Globalization;
+
+namespace Retorna_Preco
+{
+    class Produto
+    {
+        public int Id { get; set; }
+        public int Qtd { get; set; }
+        private double _vlTotal;
+        private double _vlSubTotal;
+
+
+        //CONSTRUTORES
+        public Produto ()
+        {
+
+        }
+        public Produto (int id, int qtd)
+        {
+            Id = id;
+            Qtd = qtd;
+            //ValorProdutos(id, qtd);
+        }
+        //METODOS
+        public double CalculaValor(int id, int qtd)
+        {
+            //Soma total com o calculo do valor do produto
+            _vlTotal = _vlTotal + ValorProdutos(id, qtd); 
+            return _vlTotal;
+        }
+
+        public double ValorProdutos(int id, int qtd)
+        {
+            //-------------------------------------
+            //METODO PARA CALCULAR VALOR DO PRODUTO
+            //-------------------------------------
+            /* if (id == 1)
+             {
+                 //Rosa Monique
+                 double _rosaMonique = 0.1;
+                 _vlSubTotal = _vlSubTotal + (_rosaMonique * qtd);
+             }
+             else if (id == 2)
+             {
+                 //Sepala Pequena
+                 double _sepalaPequena = 0.05;
+                 _vlSubTotal = _vlSubTotal + (_sepalaPequena * qtd);
+             }
+             else if (id == 3)
+             {
+                 //Folha Rosa Grande
+                 double _folhaRosaGrande = 0.1;
+                 _vlSubTotal = _vlSubTotal + (_folhaRosaGrande * qtd);
+             }
+             else if (id == 4)
+             {
+                 //Folha Desidratada
+                 double _folhaDesidratada = 0.05;
+                 _vlSubTotal = _vlSubTotal + (_folhaDesidratada * qtd);
+             }
+             else if (id == 5)
+             {
+                 //Arame
+                 double _arame = 1.00;
+                 _vlSubTotal = _vlSubTotal + (_arame * qtd);
+             }
+             else if (id == 6)
+             {
+                 //Vaso Vidro
+                 double _vasoVidro = 8.00;
+                 _vlSubTotal = _vlSubTotal + (_vasoVidro * qtd);
+             }
+             else if (id == 7)
+             {
+                 //Fita Floral
+                 double _fitaFloral = 0.20;
+                 _vlSubTotal = _vlSubTotal + (_fitaFloral * qtd);
+             }
+             return _vlSubTotal;    
+             */
+            switch (id)
+            {
+                case 1:
+                    //Rosa Monique
+                    double _rosaMonique = 0.1;
+                    _vlSubTotal = _vlSubTotal + (_rosaMonique * qtd);
+                    break;
+                case 2:
+                    //Sepala Pequena
+                    double _sepalaPequena = 0.05;
+                    _vlSubTotal = _vlSubTotal + (_sepalaPequena * qtd);
+                    break;
+                case 3:
+                    //Folha Rosa Grande
+                    double _folhaRosaGrande = 0.1;
+                    _vlSubTotal = _vlSubTotal + (_folhaRosaGrande * qtd);
+                    break;
+                case 4:
+                    //Folha Desidratada
+                    double _folhaDesidratada = 0.05;
+                    _vlSubTotal = _vlSubTotal + (_folhaDesidratada * qtd);
+                    break;
+                case 5:
+                    //Arame
+                    double _arame = 1.00;
+                    _vlSubTotal = _vlSubTotal + (_arame * qtd);
+                    break;
+                case 6:
+                    //Vaso Vidro
+                    double _vasoVidro = 8.00;
+                    _vlSubTotal = _vlSubTotal + (_vasoVidro * qtd);
+                    break;
+                case 7:
+                    //Fita Floral
+                    double _fitaFloral = 0.20;
+                    _vlSubTotal = _vlSubTotal + (_fitaFloral * qtd);
+                    break;
+                default:
+                    _vlSubTotal = _vlSubTotal + 0;
+                    break;
+            }
+            return _vlSubTotal;
+        }
+        //Sobreposição        
+        public override string ToString()
+        {
+            return "SubTotal: "
+                 + _vlTotal.ToString("F2", CultureInfo.InvariantCulture);
+
+        }
+    }
+}
--- Retorna_Preco/Program.cs
+++ Retorna_Preco/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Globalization;
+
 
 namespace Retorna_Preco
 {
@@ -6,7 +8,43 @@ namespace Retorna_Preco
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double vlTotal =0;
+            //double vlSubTotal;            
+           // Produto p = new Produto();
+            Console.WriteLine("Deseja lançar produtos?" + "\n"
+                             + "(1) SIM / (0) NÃO");
+            int n = int.Parse(Console.ReadLine());
+            while (n != 0)
+            {
+                double vlSubTotal;
+                //------------
+                Console.WriteLine("Escolha os produtos:");
+                Console.WriteLine("(1) Rosa Monique" + "\n"
+                                + "(2) Sepala Pequena" + "\n"
+                                + "(3) Folha de Rosa Grande" + "\n"
+                                + "(4) Flor Desidratada" + "\n"
+                                + "(5) Arame" + "\n"
+                                + "(6) Vaso de Vidro" + "\n"
+                                + "(7) Fita Floral");
+                int id_produto = int.Parse(Console.ReadLine());
+                Console.WriteLine("Entre com a quantidade:");
+                int qt_produto = int.Parse(Console.ReadLine());
+
+                Produto p = new Produto(id_produto, qt_produto);                
+                vlSubTotal = p.CalculaValor(id_produto, qt_produto);
+                //----
+                vlTotal = vlTotal + vlSubTotal;
+                Console.WriteLine("Deseja lançar produtos?" + "\n"
+                                + "(1) SIM / (0) NÃO");
+                n = int.Parse(Console.ReadLine());
+                Console.WriteLine("-------------------------");
+                Console.WriteLine(p);
+                Console.WriteLine("-------------------------");
+            }
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Valor Total: " + vlTotal.ToString("F2", CultureInfo.InvariantCulture));
+            
         }
     }
 }
